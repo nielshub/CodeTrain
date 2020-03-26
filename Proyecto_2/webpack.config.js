bd5ba90396c63a60fc9c032d7cf1190289cfba61
nielshub <niels.sanchez@gmail.com>
2020-03-26 01:19:53 +0100

    Adding proyecto 2 webpack part NOTFIXED

---
+++ Proyecto_2/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  plugins: [new CopyWebpackPlugin([{ from: "src/images", to: "images" }])],
+};
